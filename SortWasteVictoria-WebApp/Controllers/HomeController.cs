5a328dd88ce2c12da0fefa2c414126ef2d33af21
Jonathan Zhu <jhzhu3@student.monash.edu>
2023-03-11 15:48:40 +1100

    [Jonathan][Added Information Page]

--- SortWasteVictoria-WebApp/Controllers/HomeController.cs
+++ SortWasteVictoria-WebApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SortWasteVictoria_WebApp.Controllers
             return View();
         }
 
+        public IActionResult InformationPage() 
+        { 
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
